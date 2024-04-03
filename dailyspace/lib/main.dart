7770afc9e77cbcca1ed47316990309dd2dc53041
Zheng <s232281@student.dtu.dk>
2024-04-03 19:06:53 +0200

    timer

--- dailyspace/lib/main.dart
+++ dailyspace/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
+String _formatDuration(Duration d) {
+  String twoDigits(int n) => n.toString().padLeft(2, "0");
+  String twoDigitHours = twoDigits(d.inHours);
+  String twoDigitMinutes = twoDigits(d.inMinutes.remainder(60));
+  String twoDigitSeconds = twoDigits(d.inSeconds.remainder(60));
+  return "$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds";
+}
+
+class Activity {
+  String name;
+  DateTime startTime;
+  Timer? countdownTimer;
+  Duration remainingTime = Duration(minutes: 1);
+
+  Activity({required this.name, required this.startTime});
+
+  void startCountdown(
+      void Function() onFinish, void Function(Duration) onTick) {
+    final oneSec = Duration(seconds: 1);
+    remainingTime = Duration(minutes: 1);
+    countdownTimer?.cancel();
+    countdownTimer = Timer.periodic(oneSec, (Timer timer) {
+      if (remainingTime == Duration(seconds: 0)) {
+        timer.cancel();
+        onFinish();
+      } else {
+        remainingTime = remainingTime - oneSec;
+        onTick(remainingTime);
+      }
+    });
+  }
+}
+
 class ActivityTracker extends StatefulWidget {
   const ActivityTracker({Key? key}) : super(key: key);
 
@@ -24,13 +58,28 @@ class ActivityTracker extends StatefulWidget {
 }
 
 class _ActivityTrackerState extends State<ActivityTracker> {
-  List<String> availableActivities =
-      List.generate(40, (index) => 'Activity ${index + 1}');
-  List<String> activeActivities = [];
+  List<Activity> availableActivities = [];
+  List<Activity> activeActivities = [];
+  List<Activity> beginActivities = [];
+  List<Activity> endActivities = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DateTime currentTime = DateTime.now();
+    availableActivities = List.generate(10, (index) {
+      return Activity(
+          name: 'Activity ${index + 1}',
+          startTime: currentTime.add(Duration(hours: index)));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Activity Tracker'),
+      ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -72,21 +121,44 @@ class _ActivityTrackerState extends State<ActivityTracker> {
                 scrollDirection: Axis.horizontal,
                 itemCount: activeActivities.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Container(
-                      height: 100,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        color: Colors.orangeAccent,
-                        borderRadius: BorderRadius.circular(10),
+                  return LongPressDraggable<Activity>(
+                    data: activeActivities[index],
+                    feedback: Material(
+                      child: Container(
+                        height: 100,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            activeActivities[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
                       ),
-                      child: Center(
-                        child: Text(
-                          activeActivities[index],
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
+                    ),
+                    childWhenDragging: Container(),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        height: 100,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.orangeAccent,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            activeActivities[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
@@ -104,26 +176,48 @@ class _ActivityTrackerState extends State<ActivityTracker> {
                           vertical: 20, horizontal: 20),
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.blue
-                              .withOpacity(0.8), // Transparent blue color
+                          color: Colors.blue.withOpacity(0.8),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: DragTarget<String>(
+                        child: DragTarget<Activity>(
                           builder: (context, candidateData, rejectedData) {
-                            return const Center(
-                              child: Text(
-                                "Begin Activity",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
+                            return ListView.builder(
+                              itemCount: beginActivities.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        beginActivities[index].name,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      Text(
+                                        _formatDuration(beginActivities[index]
+                                            .remainingTime),
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
                             );
                           },
                           onAccept: (data) {
                             setState(() {
-                              activeActivities.add(data);
-                              availableActivities.remove(data);
+                              beginActivities.add(data);
+                              availableActivities.removeWhere(
+                                  (activity) => activity.name == data.name);
+                              data.startCountdown(() {
+                                setState(() {
+                                  activeActivities.add(data);
+                                  beginActivities.removeWhere(
+                                      (activity) => activity.name == data.name);
+                                });
+                              }, (remainingTime) {
+                                setState(() {});
+                              });
                             });
                           },
                         ),
@@ -136,18 +230,30 @@ class _ActivityTrackerState extends State<ActivityTracker> {
                           vertical: 20, horizontal: 20),
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.green
-                              .withOpacity(0.8), // Transparent green color
+                          color: Colors.green.withOpacity(0.8),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: const Center(
-                          child: Text(
-                            "End Activity",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
+                        child: DragTarget<Activity>(
+                          builder: (context, candidateData, rejectedData) {
+                            return ListView.builder(
+                              itemCount: endActivities.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title: Text(
+                                    endActivities[index].name,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          onAccept: (Activity data) {
+                            setState(() {
+                              endActivities.add(data);
+                              activeActivities.removeWhere(
+                                  (activity) => activity.name == data.name);
+                            });
+                          },
                         ),
                       ),
                     ),
@@ -167,27 +273,29 @@ class _ActivityTrackerState extends State<ActivityTracker> {
                 scrollDirection: Axis.horizontal,
                 itemCount: availableActivities.length,
                 itemBuilder: (context, index) {
-                  return LongPressDraggable<String>(
+                  return LongPressDraggable<Activity>(
                     data: availableActivities[index],
-                    feedback: Container(
-                      height: 100,
-                      width: 150,
-                      margin: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.tealAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          availableActivities[index],
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
+                    feedback: Material(
+                      child: Container(
+                        height: 100,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.tealAccent,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            availableActivities[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
                     ),
                     childWhenDragging: Container(),
+                    onDragCompleted: () {},
                     child: Container(
                       height: 100,
                       width: 150,
@@ -198,7 +306,7 @@ class _ActivityTrackerState extends State<ActivityTracker> {
                       ),
                       child: Center(
                         child: Text(
-                          availableActivities[index],
+                          availableActivities[index].name,
                           style: const TextStyle(
                             color: Colors.white,
                             fontSize: 16,
