9588a675db08d21ea92db8f957f9c28c07df10c1
Zheng <s232281@student.dtu.dk>
2024-04-22 22:04:25 +0200

    scrollable calendar

--- dailyspace/lib/screens/calendar.dart
+++ dailyspace/lib/screens/calendar.dart
@@ -9,10 +9,7 @@ class Calendar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, TaskInfo> activities =
-        ModalRoute.of(context)?.settings.arguments as Map<String, TaskInfo>? ??
-            availableActivities ??
-            {};
+    final Map<String, TaskInfo> activities = availableActivities ?? {};
 
     return Scaffold(
       appBar: AppBar(
@@ -20,36 +17,67 @@ class Calendar extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: activities.isEmpty
-          ? Center(
-              child: Text('No activities available'),
-            )
-          : buildActivitiesList(activities),
+          ? Center(child: Text('No activities available'))
+          : buildActivitiesTimeline(activities),
     );
   }
 
-  Widget buildActivitiesList(Map<String, TaskInfo> activities) {
-    return ListView.builder(
-      itemCount: activities.length,
-      itemBuilder: (context, index) {
-        TaskInfo task = activities.values.elementAt(index);
-        return Card(
-          child: ListTile(
-            title: Text(task.title),
-            subtitle: Text(
-                'Start: ${formatDateTime(task.start)} - End: ${formatDateTime(task.end)}\nDuration: ${calculateDuration(task.start, task.end)}'),
-            isThreeLine: true,
+  Widget buildActivitiesTimeline(Map<String, TaskInfo> activities) {
+    List<Widget> timelineTasks = [];
+    for (int hour = 0; hour < 24; hour++) {
+      List<TaskInfo> hourlyTasks = activities.values
+          .where((task) =>
+              task.start != null && DateTime.parse(task.start!).hour == hour)
+          .toList();
+
+      Widget timeLabel = Container(
+        padding: EdgeInsets.symmetric(
+            vertical: 16), // Adjust the space for each hour
+        child: Text('${hour.toString().padLeft(2, '0')}:00'),
+      );
+
+      List<Widget> taskTiles =
+          hourlyTasks.map((task) => buildTaskTile(task)).toList();
+
+      timelineTasks.add(
+        IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(width: 50, child: timeLabel),
+              Expanded(child: Column(children: taskTiles)),
+            ],
           ),
-        );
-      },
+        ),
+      );
+    }
+
+    return ListView(children: timelineTasks);
+  }
+
+  Widget buildTaskTile(TaskInfo task) {
+    return Flexible(
+      child: Container(
+        padding: EdgeInsets.all(4.0),
+        child: ListTile(
+          title: Text(
+            task.title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+            overflow: TextOverflow.ellipsis,
+          ),
+          subtitle: Text(
+            '${formatDateTime(task.start)} - ${formatDateTime(task.end)}',
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ),
     );
   }
 
   String formatDateTime(String? dateTimeStr) {
-    if (dateTimeStr == null) {
-      return 'Unknown time';
-    }
+    if (dateTimeStr == null) return 'Unknown time';
     DateTime dateTime = DateTime.parse(dateTimeStr);
-    return DateFormat('yyyy-MM-dd HH:mm').format(dateTime); // 格式化时间
+    return DateFormat('yyyy-MM-dd HH:mm').format(dateTime);
   }
 
   String calculateDuration(String? start, String? end) {
