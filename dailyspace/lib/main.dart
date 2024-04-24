cff71b30c58cf2e1151ae4bead428ce9f9d3ac48
Frederik Peetz-Schou Larsen <s174159@student.dtu.dk>
2024-04-24 09:30:46 +0200

    Added graph library to dependencies

--- dailyspace/lib/main.dart
+++ dailyspace/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:dailyspace/screens/vis.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart'; // Import your login screen widget
 import 'package:firebase_core/firebase_core.dart';
@@ -12,13 +16,63 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(), // Open the login screen first
+      title: 'KindaCode.com',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+// Define data structure for a bar group
+class DataItem {
+  int x;
+  double y1;
+  double y2;
+  double y3;
+  DataItem(
+      {required this.x, required this.y1, required this.y2, required this.y3});
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  // Generate dummy data to feed the chart
+  final List<DataItem> _myData = List.generate(
+      30,
+      (index) => DataItem(
+            x: index,
+            y1: Random().nextInt(20) + Random().nextDouble(),
+            y2: Random().nextInt(20) + Random().nextDouble(),
+            y3: Random().nextInt(20) + Random().nextDouble(),
+          ));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Piechart'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: PieChart(PieChartData(
+              centerSpaceRadius: 5,
+              borderData: FlBorderData(show: false),
+              sectionsSpace: 2,
+              sections: [
+                PieChartSectionData(
+                    value: 35, color: Colors.purple, radius: 100),
+                PieChartSectionData(
+                    value: 40, color: Colors.amber, radius: 100),
+                PieChartSectionData(
+                    value: 55, color: Colors.green, radius: 100),
+                PieChartSectionData(
+                    value: 70, color: Colors.orange, radius: 100),
+              ]))),
     );
   }
 }
